c9522ba9bc8f51da52a0d01a777ae83eefa46c68
Prasanna B koppal <113998437+Prasanna-koppal@users.noreply.github.com>
2023-07-29 14:29:27 +0530

    Create Second_Largest.java
    Day 1 problem solved

---
+++ Day_1/Second_Largest.java
@@ -0,0 +1,33 @@
+package second_largest;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Second_largest {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+        System.out.println("Enter length of Array");
+        int n=scan.nextInt();
+        int temp;
+        int arr[] = new int[n];
+        int size=arr.length;
+        for (int i = 0 ;i<size;i++){
+            arr[i]=scan.nextInt();
+        }
+        //using inbuilt function to sort an array
+        Arrays.sort(arr);
+        int largest=arr[size-1];
+        
+        int second_largest=-1;
+        for(int i=0; i<size; i++){
+           if(arr[i]>second_largest && arr[i]!=largest){
+               second_largest=arr[i];
+           }
+        }
+        System.out.println("Second Largest:"+second_largest);
+scan.close();
+
+	}
+
+}
